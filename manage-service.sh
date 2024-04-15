acaa548097005a072f3edddbbfbf937816f3e265
Vijay Balasubramaniam <vbalasu@gmail.com>
2024-04-14 21:29:17 -0700

    Added systemd unit

---
+++ manage-service.sh
@@ -0,0 +1,5 @@
+#sudo cp chatbot.service /etc/systemd/system/chatbot.service
+#sudo systemctl daemon-reload
+#sudo systemctl start chatbot.service
+#sudo systemctl enable chatbot.service
+sudo systemctl status chatbot.service
