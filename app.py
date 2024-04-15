2200e39c7700888b3d9608129aea7f28456bf4e6
Vijay Balasubramaniam <vbalasu@gmail.com>
2024-04-14 20:52:08 -0700

    Added option for DBRX

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ import chainlit as cl
 from dotenv import load_dotenv
 load_dotenv()
 from langchain_community.chat_models.ollama import ChatOllama
+from langchain_community.chat_models.databricks import ChatDatabricks
 from langchain.memory import ConversationBufferMemory
 from langchain.schema.output_parser import StrOutputParser
+import os
 memory = ConversationBufferMemory()
 
-llm = ChatOllama(model='mixtral', stream=True)
+if('DATABRICKS_TOKEN' in os.environ):
+    llm = ChatDatabricks(endpoint='databricks-dbrx-instruct', stream=True)
+else:
+    llm = ChatOllama(model='mixtral', stream=True)
 chain = llm | StrOutputParser()
 
 @cl.on_chat_start
